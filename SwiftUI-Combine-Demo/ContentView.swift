ad3112711d57a0b7d5f071a031924274322dfedb
“Mahmoud-Abdelwahab” <mahmoudabdelwahab199390@gmail.com>
2021-07-18 08:53:46 +0200

    Initial Commit

---
+++ SwiftUI-Combine-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Combine-Demo
+//
+//  Created by Mahmoud Abdul-Wahab on 18/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
